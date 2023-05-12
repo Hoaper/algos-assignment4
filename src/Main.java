d5542fc979134c040d73bf71807afe048ea3f939
zh0per <beeboplay@gmail.com>
2023-05-12 09:21:40 +0600

    made hashtable

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        HashTable hashTable = new HashTable<String, Integer>();
+        hashTable.put("hello", 1);
+        hashTable.put("h", 2);
+        hashTable.put("e", 3);
+        hashTable.put("5", 5);
+    }
+}
