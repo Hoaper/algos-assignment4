b89e81ed68453994cabf171c0dd640cec6f75741
zh0per <beeboplay@gmail.com>
2023-05-12 09:36:38 +0600

    testing added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.util.Random;
+
+class MyTestingClass{
+    private int val;
+    public MyTestingClass(int v) {
+        this.val = v;
+    }
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(val);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        HashTable hashTable = new HashTable<String, Integer>();
-        hashTable.put("hello", 1);
-        hashTable.put("h", 2);
-        hashTable.put("e", 3);
-        hashTable.put("5", 5);
+        HashTable hashTable = new HashTable<MyTestingClass, String>();
+        Random rand = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int val = rand.nextInt(0, 9999999);
+            hashTable.put(new MyTestingClass(val), "Student " + val);
+        }
+
     }
 }
