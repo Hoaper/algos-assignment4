c9258ffb943be72026618beca20495ab08dc96ee
zh0per <beeboplay@gmail.com>
2023-05-19 09:09:04 +0600

    Showcase for replace method of hashtable added

--- src/Main.java
+++ src/Main.java
@@ -15,11 +15,16 @@ public class Main {
     public static void main(String[] args) {
         HashTable hashTable = new HashTable<MyTestingClass, String>();
         Random rand = new Random();
-        for (int i = 0; i < 10000; i++) {
-            int val = rand.nextInt(0, 9999999);
-            hashTable.put(new MyTestingClass(val), "Student " + val);
-        }
+//        for (int i = 0; i < 10000; i++) {
+//            int val = rand.nextInt(0, 9999999);
+//            hashTable.put(new MyTestingClass(val), "Student " + val);
+//        }
+//
+//        System.out.println(hashTable);
 
-        System.out.println(hashTable);
+        MyTestingClass key = new MyTestingClass(1337);
+        hashTable.put(key, "Test case");
+        hashTable.replace(key, "New Test case", "Test case");
+        System.out.println(hashTable.get(key));
     }
 }
